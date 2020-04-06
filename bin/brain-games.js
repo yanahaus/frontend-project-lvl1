33a8a7555b448c1b62a399afb798d8cce9027119
Yana Khaustova <yana.haus@gmail.com>
2020-04-06 19:43:48 +0300

    step3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!')
+import readlineSync from '../src/index.js';
+
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name?');
+console.log('Hello, ' + userName + '!');
+
+
